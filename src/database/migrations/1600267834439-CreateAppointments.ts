2bbc1fe7f8fe97887a4d6aa34878a1a94f2d3425
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-17 17:43:35 -0300

    Ajustes nas migrations e models

--- src/database/migrations/1600267834439-CreateAppointments.ts
+++ src/database/migrations/1600267834439-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1600267834439
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
           {
             name: 'created_at',
