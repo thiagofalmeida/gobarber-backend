87fe233631546143422076c34d1471d575d357dc
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-17 18:15:03 -0300

    Criado relação entre as tabelas, alterado coluna na base

--- src/database/migrations/1600373897667-CreateUsers.ts
+++ src/database/migrations/1600373897667-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1600373897667 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
