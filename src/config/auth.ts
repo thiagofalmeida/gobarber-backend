71d827eea9eb6bc21a9c64865ada8924bee05c63
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-18 16:24:48 -0300

    Add .env file

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   jwt: {
-    secret: '04f38d5f332a9410d3aa737eff3b2698',
-    expiresIn: '1d',
+    secret: process.env.JWT_SECRET as string,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
+import * as dotenv from 'dotenv';
 import routes from './routes';
 import './database';
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(routes);
