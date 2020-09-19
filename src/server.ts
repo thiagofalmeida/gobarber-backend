d0fdccc5fe11d3853bd4a04db69116817cf2cf91
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-18 23:32:24 -0300

    Lidando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import * as dotenv from 'dotenv';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -14,6 +16,20 @@ dotenv.config();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'message',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server Started!');
