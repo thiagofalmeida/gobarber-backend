84d93aa94fd8df747015705227cbaa892d961f48
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-05 16:18:38 -0300

    Injeção de dependências com tsyringe

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
 import UserMap from '@modules/users/dtos/UserMap';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const userRepository = new UsersRepository();
-  const authenticateUser = new AuthenticateUserService(userRepository);
+  const authenticateUser = container.resolve(AuthenticateUserService);
 
   const { user, token } = await authenticateUser.execute({
     email,
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { container } from 'tsyringe';
 
 import UserMap from '@modules/users/dtos/UserMap';
 import uploadConfig from '@config/upload';
 import ensureAnthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -14,8 +14,7 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
+  const createUser = container.resolve(CreateUserService);
 
   const user = await createUser.execute({
     name,
@@ -33,8 +32,7 @@ usersRouter.patch(
   ensureAnthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
