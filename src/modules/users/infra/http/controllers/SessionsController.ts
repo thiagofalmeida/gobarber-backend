5fe7f8bc4d5f61fb916e5bdde02fec381ab0ea4d
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-05 16:40:38 -0300

    removido comentarios

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-// import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import UserMap from '@modules/users/dtos/UserMap';
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-// import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import UserMap from '@modules/users/dtos/UserMap';
