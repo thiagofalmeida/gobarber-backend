48e7e3539f3154404075d05364c64d2c5c601679
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-07 20:41:14 -0300

    Finalizando testes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -18,7 +18,7 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
