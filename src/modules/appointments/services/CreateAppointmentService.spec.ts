0c38ce2cb5adf86c6fe1e43982a06ba6a26b72a5
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-07 09:36:09 -0300

    Coverage Report

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  // it('should not be able to create a two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
