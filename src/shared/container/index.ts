ceb75dac68332bdecc0bb50b4fd9a7dbe037ab81
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-14 21:54:44 -0300

    Enviado notificações

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepos
 import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
 import UsersTokenRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
 
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
@@ -25,4 +28,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUsersTokensRepository>(
   'UsersTokenRepository',
   UsersTokenRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
 );
