204a1855bca6f500811843939a7a0e28b6d1468e
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-16 11:27:43 -0300

    Configurando cache

---
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -0,0 +1,14 @@
+import { container } from 'tsyringe';
+import cacheConfig from '@config/cache';
+
+import RedisCacheProvider from './implementations/RedisCacheProvider';
+import ICacheProvider from './models/ICacheProvider';
+
+const providers = {
+  redis: RedisCacheProvider,
+};
+
+container.registerSingleton<ICacheProvider>(
+  'CacheProvider',
+  providers[cacheConfig.driver],
+);
