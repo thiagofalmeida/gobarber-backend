224efe5c6296d624a13be432ba75b1a2675b88ce
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-10-19 14:22:28 -0300

    Corrigido problema com agendamento no mesmo horário

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 interface IAppointmentsRepository {
   create(date: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
